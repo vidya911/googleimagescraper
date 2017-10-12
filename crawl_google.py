cd954df6435e9f4eac6e5c3bb37854f003640083
Vidya Sagar <sagarv@vmware.com>
2017-10-12 20:24:58 +0530

    removed white spaces and autopep-8

--- crawl_google.py
+++ crawl_google.py
@@ -9,31 +9,32 @@ from pyvirtualdisplay import Display
 
 display = Display(visible=0, size=(800, 600))
 display.start()
+
+
 def visit_walk_google(search_string):
+    """
 
-    """ 
-
-    Open google.com and search for funny images  
+    Open google.com and search for funny images
 
     """
 
-    driver = webdriver.Firefox() 
+    driver = webdriver.Firefox()
     driver.maximize_window()
     driver.get(SITE_URL)
     driver.implicitly_wait(3)
     search_box = driver.find_element_by_name('q')
     search_box.send_keys(search_string)
     driver.find_element_by_name('btnG').click()
-    get_inside_image_search_page(driver,search_string)
+    get_inside_image_search_page(driver, search_string)
 
-def get_inside_image_search_page(driver,search_string):
 
-    """ 
-    Get all search link from the main search page with desired images 
-    
+def get_inside_image_search_page(driver, search_string):
+    """
+    Get all search link from the main search page with desired images
+
     """
 
-    #driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[4]/div[3]/div/div/div[1]/div/div/div[2]/a').click()
+    # driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[4]/div[3]/div/div/div[1]/div/div/div[2]/a').click()
     all_header_menu = driver.find_elements_by_class_name('qs')
     for each in all_header_menu:
         if each.text == 'Images':
@@ -45,20 +46,20 @@ def get_inside_image_search_page(driver,search_string):
     check_more_images(driver)
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
-    all_image_soup = soup.findAll("a",{"class":"rg_l"}) 
+    all_image_soup = soup.findAll("a", {"class": "rg_l"})
     all_images_links = get_all_images_links(all_image_soup)
     filename = '-'.join(search_string.split(' ')) + '_images.txt'
-    f = open(filename,'wb')
+    f = open(filename, 'wb')
     for each_image in all_images_links:
         f.write(each_image)
         f.write('\n')
     f.close()
 
+
 def get_all_images_links(all_image_soup):
+    """
 
-    """ 
 
-    
     """
 
     image_urls = []
@@ -67,20 +68,24 @@ def get_all_images_links(all_image_soup):
             image_unparsed_link = each_image.attrs.get('href')
             image_url = get_image_url(image_unparsed_link)
             image_extension = image_url.split('.')[-1]
-            if image_extension.lower() in ['jpg','png','gif']:
+            if image_extension.lower() in ['jpg', 'png', 'gif']:
                 image_urls.append(image_url)
         except:
             pass
     return image_urls
 
+
 def check_more_images(driver):
     try:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
         driver.find_element_by_id('smb').click()
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
         check_more_images(driver)
     except:
-        return #driver 
+        return  # driver
+
 
 def get_image_url(image_unparsed_link):
     image_url = image_unparsed_link.split('imgurl=')[1].split('&')[0]
